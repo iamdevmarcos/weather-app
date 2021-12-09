b211f0301629c494acf737f8fea8dc3f2d2b3413
Marcos Andre <marcosdev.me@gmail.com>
2021-12-09 11:39:59 -0300

    creating version 0.1

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+const App = () => {
+  return(
+    <div>
+      <h1>Clima</h1>
+
+      <form className="busca">
+          <input type="search" id="searchInput" />
+          <button>Buscar</button>
+      </form>
+
+      <div className="resultado">
+          <div className="titulo">--</div>
+
+          <div className="info">
+              <div className="temp">
+                  <div className="tempTitulo">Temperatura</div>
+                  <div className="tempInfo">-- <sup>ºC</sup></div>
+                  <img src="http://openweathermap.org/img/wn/10d@2x.png" />
+              </div>
+              <div className="vento">
+                  <div className="ventoTitulo">Vento</div>
+                  <div className="ventoInfo">-- <span>km/h</span></div>
+                  <div className="ventoArea">
+                      <div className="ventoPonto"></div>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <div className="aviso"></div>
+
+      <footer>Criado pela </footer>
+    </div>
+  );
+}
+
+export default App;

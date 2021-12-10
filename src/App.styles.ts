90b4db4fc369958f784b66454d7de2135fef5abf
Marcos Andre <marcosdev.me@gmail.com>
2021-12-09 21:41:46 -0300

    adding keyboard event and font in css

---
+++ src/App.styles.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const Key = styled.span`
+    font-family: Consolas !important;
+    background-color:#EDF2F7;
+    border-radius: 5px;
+    border-bottom: 3px solid #CCC;
+    border-bottom-width:3px;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: normal;
+    padding:5px;
+    padding-inline: 0.6em;
+    white-space: nowrap;
+    margin:0px 5px;
+    transition: all ease 0.2s;
+    color:#000;
+    &&:hover {
+        background-color:#CCC;
+        border-bottom-width:1px;
+        cursor:pointer;
+    }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { useState, useEffect } from 'react';
+import * as C from './App.styles';
 import { WeatherArea } from './components/WeatherArea';
-import showMessage from './helpers/showMessage';
 
 const App = () => {
+  const [pressEnter, setPressEnter] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('keydown', function(e) {
+      switch(e.code) {
+        case 'Enter':
+        case 'NumpadEnter':
+          setPressEnter(true);
+        break;
+      }
+    })
+  });
 
   return(
     <div>
-      <WeatherArea />
+
+      {!pressEnter &&
+        <div className="warning">
+          Press
+            <C.Key onClick={e=>setPressEnter(true)}>enter</C.Key> 
+          to start
+        </div>
+      }
+
+      {pressEnter &&
+        <WeatherArea />
+      }
     </div>
   );
 }

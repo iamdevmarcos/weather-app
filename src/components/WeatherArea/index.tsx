fdb5518bdc60d3f2a79d59a5b695be8add2b5f47
Marcos Andre <marcosdev.me@gmail.com>
2021-12-09 21:07:46 -0300

    adjusting weather component

--- src/components/WeatherArea/index.tsx
+++ src/components/WeatherArea/index.tsx
@@ -4,27 +4,31 @@ import showMessage from '../../helpers/showMessage';
 
 export const WeatherArea = () => {
     const [cityName, setCityName] = useState('');
+    const [infoAreaVisible, setInfoAreaVisible] = useState(false);
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
     
         if(cityName !== '') {
+            setInfoAreaVisible(true)
             const result = await api.getWeatherInfo(cityName);
         
             if(result) {
                 console.log('achou', result);
             } else {
-                showMessage('City not found....');
+                setInfoAreaVisible(false);
+                showMessage(`City ${cityName} not found...`);
             }
 
         } else {
+            setInfoAreaVisible(false);
             showMessage('Field cannot be empty');
         }
     }
 
     return (
         <div>
-            <h1>Clima</h1>
+            <h1>⛅️ Weather App</h1>
 
             <form className="busca" onSubmit={handleSubmit}>
                 <input
@@ -32,30 +36,31 @@ export const WeatherArea = () => {
                     id="searchInput"
                     value={cityName}
                     onChange={e=>setCityName(e.target.value)}
+                    placeholder="search for a city"
                 />
                 <button>Search</button>
             </form>
 
-            <div className="resultado">
-                <div className="titulo">--</div>
+            {infoAreaVisible &&
+                <div className="resultado">
+                    <div className="titulo">--</div>
 
-                <div className="info">
-                    <div className="temp">
-                        <div className="tempTitulo">Temperatura</div>
-                        <div className="tempInfo">-- <sup>ºC</sup></div>
-                        <img src="http://openweathermap.org/img/wn/10d@2x.png" />
-                    </div>
-                    <div className="vento">
-                        <div className="ventoTitulo">Vento</div>
-                        <div className="ventoInfo">-- <span>km/h</span></div>
-                        <div className="ventoArea">
-                            <div className="ventoPonto"></div>
+                    <div className="info">
+                        <div className="temp">
+                            <div className="tempTitulo">Temperatura</div>
+                            <div className="tempInfo">-- <sup>ºC</sup></div>
+                            <img src="http://openweathermap.org/img/wn/10d@2x.png" />
+                        </div>
+                        <div className="vento">
+                            <div className="ventoTitulo">Vento</div>
+                            <div className="ventoInfo">-- <span>km/h</span></div>
+                            <div className="ventoArea">
+                                <div className="ventoPonto"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div className="aviso"></div>
+            }
 
             <footer>
                 Made with 🤍 by <a href="https://github.com/iamdevmarcos" target="_blank">Marcos Andre</a>

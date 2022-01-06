951b4e1c763229631cb0a00c88ade047adffcc32
Marcos Andre <marcosdev.me@gmail.com>
2022-01-06 06:46:49 -0300

    feat: showing data in console

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,6 +8,7 @@ const api = {
     getWeatherInfo: async (city: string) => {
         try {
             const res = await http.get(`/data/2.5/weather?units=metric&lang=pt_br&appid=1ab436dd24aa2653bda5814f3390002a&q=${encodeURI(city)}`);
+            console.log(res.data);
             return res.data;
         } catch(e) {
             console.log('error', e);
